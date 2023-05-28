1faaf190ba6e7beea19b56d9586ef8e87f5b04ce
KyleD0711 <97480794+KyleD0711@users.noreply.github.com>
2023-05-28 14:45:18 -0500

    Created all of the routes and updated the controller to support all of the functions for the Alias and Emergency Contact

--- app/controllers/emergencyContact.controller.js
+++ app/controllers/emergencyContact.controller.js
@@ -45,3 +45,108 @@ exports.create = (req, res) => {
             });
         });
 };
+
+exports.findAll = (req, res) => {
+    const id = req.query.id;
+    var condition = id ? { id: { [Op.like]: '%${id}%' } } : null;
+
+    EmergencyContact.findAll({ where: condition })
+    .then((data) => {
+        res.send(data);
+    })
+    .catch((err) => {
+        res.status(500).send({
+            message: err.message || "An error has occurred while retrieving emergency contacts.",
+        });
+    });
+};
+
+exports.findOne = (req, res) => {
+    const id = req.params.id;
+
+    EmergencyContact.findOne({
+        where: { id: id}
+    })
+    .then((data) => {
+        if (data){
+            res.send(data);
+        }
+        else {
+            res.status(404).send({
+                message: "Cannot find Alias with id=${id}."
+            })
+        }
+    })
+    .catch((err) => {
+        res.status(500).send({
+            message: err.message || "Something went wrong while retrieving Emergency Contact.",
+        });
+    });
+};
+
+exports.update = (req, res) => {
+    const id = req.params.id;
+
+    EmergencyContact.update(req.body, {
+        where: { id: id },
+    })
+    .then((num) => {
+        if (num == 1) {
+            res.send({
+                message: "Emergency Contact was updated successfully.",
+            });
+        }
+        else {
+            res.send({
+                message: "Cannot update Emergency Contact with id=${id}",
+            })
+        }
+    })
+    .catch((err) => {
+        res.status(500).send({
+            message: err.message || "An error occurred while trying to update Emergency Contact",
+        })
+    })
+}
+
+exports.delete = (req, res) => {
+    const id = req.params.id;
+
+    EmergencyContact.destroy({
+        where: { id: id },
+    })
+    .then((num) => {
+        if (num == 1) {
+            res.send( {
+                message: "Emergency Contact was deleted successfully.",
+            })
+        }
+        else {
+            res.send( {
+                message: "There was a problem deleting Emergency Contact.",
+            })
+        }
+    })
+    .catch((err) => {
+        res.status(500).send({
+            message: err.message || "There was an error deleteing Emergency Contact.",
+        });
+    });
+};
+
+exports.deleteAll = (req, res) => {
+    EmergencyContact.destroy({
+        where: {},
+        truncate: false,
+    })
+    .then((nums) => {
+        res.send({
+            message: '${nums} Emergency Contacts were deleted successfully.',
+        })
+    })
+    .catch((err) => {
+        res.status(500).send({
+            message: err.message || "There was an error while removing all emergency contacts.",
+        });
+    });
+};
