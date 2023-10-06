5e4bd9c597fa91ecf19bdb13f16599ebab1c7f8e
Frostbit912 <97480950+JonathanBooker912@users.noreply.github.com>
2023-10-06 12:19:14 -0500

    Added functionality for title maintenance

--- app/controllers/title.controller.js
+++ app/controllers/title.controller.js
@@ -30,6 +30,41 @@ exports.create = (req, res) => {
     });
 };
 
+exports.update = (req, res) => {
+  const { id } = req.params;
+
+  // Validate request
+  if (!req.body.name) {
+    res.status(400).send({
+      message: "Content can not be empty!",
+    });
+    return;
+  }
+
+  const updatedTitle = {
+    name: req.body.name,
+  };
+
+  Title.update(updatedTitle, {
+    where: { id: id },
+  })
+    .then((result) => {
+      if (result[0] === 1) {
+        res.status(200).send({ message: "Title was updated successfully." });
+      } else {
+        res.status(404).send({
+          message: `Cannot update Title with id=${id}. Title not found.`,
+        });
+      }
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message: "Error updating Title with id=" + id,
+      });
+    });
+};
+
+
 exports.find = (req, res) => {
   const { id } = req.params;
 
@@ -60,6 +95,28 @@ exports.findAll = (req, res) => {
       res.status(500).send({
         message:
           err.message || "Some error occurred while retrieving tutorials.",
+      });
+    });
+};
+
+exports.delete = (req, res) => {
+  const { id } = req.params;
+
+  Title.destroy({
+    where: { id: id },
+  })
+    .then((result) => {
+      if (result === 1) {
+        res.send({ message: "Title was deleted successfully." });
+      } else {
+        res.status(404).send({
+          message: `Cannot delete Title with id=${id}. Title not found.`,
+        });
+      }
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message: "Error deleting Title with id=" + id,
       });
     });
 };
